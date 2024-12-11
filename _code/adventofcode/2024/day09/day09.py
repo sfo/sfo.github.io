0a8948622029e1114e68dc6842c786db71641681
Stanley F <stanley.foerster@gmail.com>
2024-12-11 15:16:07 +0100

    completed day 9 of AoC 2024

--- _code/adventofcode/2024/day09/day09.py
+++ _code/adventofcode/2024/day09/day09.py
@@ -1,32 +1,73 @@
 # %%
-def decode_diskmap(map: str) -> list[int]:
-    id = 0
+def decode_sectors(sectors: list[dict[str, int]]) -> list[int]:
     decoded_map = []
-    for i in range(len(map)):
-        if i % 2 == 0:  # file
-            decoded_map += int(map[i]) * [id]
-            id += 1
-        else:  # empty
-            decoded_map += int(map[i]) * [-1]
+    for s in sectors:
+        decoded_map += s["size"] * [s["id"]] + s["free"] * [-1]
     return decoded_map
 
 
-# %%
-with open("input.txt", "rt") as file:
-    input = file.readlines()
+def checksum(diskmap: list[int]) -> int:
+    return sum([i * diskmap[i] for i in range(len(diskmap)) if diskmap[i] > 0])
+
+
+def read_sectors() -> list[dict[str, int]]:
+    with open("input.txt", "rt") as file:
+        input = file.readlines()
+
     assert len(input) == 1
-    input = decode_diskmap(input[0])
+    input = input[0]
+    return [
+        {
+            "id": id,
+            "size": int(file_size),
+            "free": int(gap_size),
+        }
+        for id, (file_size, gap_size) in enumerate(zip(input[::2], input[1::2] + "0"))
+        # add 0 to account for final "virtual" gap
+    ]
+
 
 # %%
-j = len(input) - 1
-for i in range(len(input)):
-    while input[j] == -1:
-        j -= 1
-    if i >= j:
+sectors = read_sectors()
+diskmap = decode_sectors(sectors)
+idx = len(diskmap) - 1
+for id in range(len(diskmap)):
+    while diskmap[idx] == -1:
+        idx -= 1
+    if id >= idx:
         break
-    if input[i] == -1:
-        input = input[:i] + [input[j]] + input[i+1:j] + [-1] + input[j+1:]
+    if diskmap[id] == -1:
+        diskmap = (
+            diskmap[:id]
+            + [diskmap[idx]]
+            + diskmap[id + 1 : idx]
+            + [-1]
+            + diskmap[idx + 1 :]
+        )
 
-print("Solution for part 1 is:", sum([i * input[i] for i in range(len(input)) if input[i] > 0]))
+print("Solution for part 1 is:", checksum(diskmap))
+
+# %%
+sectors = read_sectors()
+i = len(sectors) - 1
+while i > 0:
+    for idx in range(i):
+        if sectors[idx]["free"] >= sectors[i]["size"]:
+            sectors[i - 1]["free"] += sectors[i]["size"] + sectors[i]["free"]
+            sectors[i]["free"] = sectors[idx]["free"] - sectors[i]["size"]
+            sectors[idx]["free"] = 0
+            sectors = (
+                sectors[: idx + 1]
+                + [sectors[i]]
+                + sectors[idx + 1 : i]
+                + sectors[i + 1 :]
+            )
+            i += 1
+            break
+    i -= 1
+
+diskmap = decode_sectors(sectors)
+
+print("Solution for part 2 is:", checksum(diskmap))
 
 # %%
