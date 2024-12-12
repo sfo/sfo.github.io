8822075f759a346837c79e76ad380ac8172a16cf
Stanley F <stanley.foerster@gmail.com>
2024-12-12 21:41:53 +0100

    finish day 10 of AoC 2024

--- _code/adventofcode/2024/day10/day10.py
+++ _code/adventofcode/2024/day10/day10.py
@@ -4,28 +4,50 @@ import numpy as np
 
 # %%
 border_value = np.nan
-with open("mini_example.txt", "rt") as file:
+with open("input.txt", "rt") as file:
     map = np.array(list(zip(*file.readlines()))).T.astype(int)
-    map = np.concat([np.full((map.shape[0], 1), border_value), map, np.full((map.shape[0], 1), border_value)], axis=1)
-    map = np.concat([np.full((1, map.shape[1]), border_value), map, np.full((1, map.shape[1]), border_value)], axis=0)
+    map = np.concat(
+        [
+            np.full((map.shape[0], 1), border_value),
+            map,
+            np.full((map.shape[0], 1), border_value),
+        ],
+        axis=1,
+    )
+    map = np.concat(
+        [
+            np.full((1, map.shape[1]), border_value),
+            map,
+            np.full((1, map.shape[1]), border_value),
+        ],
+        axis=0,
+    )
 
 
 def search_trails(map, i, j):
-    if map[i, j] == 0:
+    if map[i, j] == 9:
         return [(i, j)]
 
     trails = []
     for d in [-1, 1]:
-        if map[i + d][j] - map[i][j] == -1:
+        if map[i + d][j] - map[i][j] == 1:
             trails += search_trails(map, i + d, j)
-        if map[i][j + d] - map[i][j] == -1:
+        if map[i][j + d] - map[i][j] == 1:
             trails += search_trails(map, i, j + d)
     return trails
 
 
+trail_heads = []
 for i in range(1, map.shape[0] - 1):
     for j in range(1, map.shape[1] - 1):
-        if map[i, j] == 9:
-            trails = search_trails(map, i, j)
+        if map[i, j] == 0:
+            trail_heads.append(search_trails(map, i, j))
 
-trails
+print(
+    "Solution for part 1 is:",
+    sum([len(set(trail)) for trail in trail_heads])
+)
+print(
+    "Solution for part 2 is:",
+    sum([len(trail) for trail in trail_heads])
+)
